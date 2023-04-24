fbf4e59b677609d480b5fa32a203a6f0f72cafed
myUsername <my>
2023-04-24 10:33:20 -0700

    C

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
-#define MAIN_H
-#include <main.h>
-int _printf(const char *format, ...);
+#fedine MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#define UNUSED(x)(void)(x)
+#define BUFFER-SIZE 1024
+int-printf(const char *format,...);
+int handle_flags(const char *format,int i);
+int handle_length(const char *format, int i );
 #endif
