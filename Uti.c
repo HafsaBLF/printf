9196358d9acbafdfc01febff4b95d20dee6f1adc
myUsername <my>
2023-04-25 06:59:57 -0700

    C

---
+++ Uti.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _isprintable - Evaluate if a char is printable
+ * @c: char
+ * Return: 1 if true, 0 if false
+*/
+int _isprintable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append ascci in hexadecimal code
+ * @buffer: array
+ * @i: int
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
