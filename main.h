96c3d3a0d897684daa6bab538937af9a1e1c99c3
HafsaBLF <Hafsablf@gmai.com>
2023-04-25 18:50:00 -0700

    DEL

--- main.h
+++ main.h
@@ -74,17 +74,4 @@ int write_unsgnd(int is_negative, int i, char buffer[],
 	int flags, int width, int precision, int len);
 int _isprintable(char);
 int append_hexa_code(char, char[], int);
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 #endif
